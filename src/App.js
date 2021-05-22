6fdf18a87ce83ca808fb4281d2a71f27a429ae54
kmac3063 <lancovlancov@mail.ru>
2021-05-22 16:15:03 +1000

    Задание 4

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot } from '@vkontakte/vkui';
+import {View, ScreenSpinner, AdaptivityProvider, AppRoot} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
-import Persik from './panels/Persik';
+import WelcomeScreen from "./panels/WelcomeScreen";
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
-	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+    const [activePanel, setActivePanel] = useState('WelcomeScreen');
+    const [fetchedUser, setUser] = useState(null);
+    const [popout, setPopout] = useState(<ScreenSpinner size='large'/>);
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			setPopout(null);
-		}
-		fetchData();
-	}, []);
+    useEffect(() => {
+        bridge.subscribe(({detail: {type, data}}) => {
+            if (type === 'VKWebAppUpdateConfig') {
+                const schemeAttribute = document.createAttribute('scheme');
+                schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+                document.body.attributes.setNamedItem(schemeAttribute);
+            }
+        });
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
-	};
+        async function fetchData() {
+            setPopout(null);
+            const user = await bridge.send('VKWebAppGetUserInfo');
+            setUser(user);
+        }
 
-	return (
-		<AdaptivityProvider>
-			<AppRoot>
-				<View activePanel={activePanel} popout={popout}>
-					<Home id='home' fetchedUser={fetchedUser} go={go} />
-					<Persik id='persik' go={go} />
-				</View>
-			</AppRoot>
-		</AdaptivityProvider>
-	);
+        async function checkAvailable() {
+            bridge.send("VKWebAppFlashGetInfo").then(
+                (p) => {
+                    if (!p.is_available) {
+                        alert("Нет фонарика")
+                    }
+                }
+            )
+        }
+
+
+
+        fetchData();
+        checkAvailable()
+    }, []);
+
+    const go = e => {
+        setActivePanel(e.currentTarget.dataset.to);
+    };
+
+    return (
+        <AdaptivityProvider>
+            <AppRoot>
+                <View activePanel={activePanel} popout={popout}>
+                    <WelcomeScreen id='WelcomeScreen' fetchedUser={fetchedUser} go={go}/>
+                    <Home id='Home' fetchedUser={fetchedUser} go={go}/>
+                </View>
+            </AppRoot>
+        </AdaptivityProvider>
+    );
 }
 
 export default App;
--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,42 +1,110 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+import {
+    Panel,
+    PanelHeader,
+    Header,
+    Button,
+    Group,
+    Cell,
+    Div,
+    Avatar,
+    SimpleCell,
+    Switch,
+    FormItem, Select
+} from '@vkontakte/vkui';
+import bridge from "@vkontakte/vk-bridge";
 
-const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+const Home = ({id, go, fetchedUser}) => {
+    const START_SIZE = 8
 
-		<Group header={<Header mode="secondary">Navigation Example</Header>}>
-			<Div>
-				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
-		</Group>
-	</Panel>
-);
+    const [bitsSize, setBitsSize] = useState(START_SIZE)
+    const [bitsInSecond, setBitsInSecond] = useState(Array.from({length: bitsSize}, (v, i) => 0))
+    const [enable, setEnable] = useState(false)
+    const [currentSecond, setCurrentSecond] = useState(0)
+    const [myInterval, setMyInterval] = useState(null)
 
-Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
-};
+    const getString = (index) => {
+        let i = index + 1
+        if (i % 10 === 3) {
+            return i + "-я секунда"
+        }
+        return i + "-ая секунда"
+    }
+
+    const getSecondOptions = () => {
+        let res = []
+        for (let i = 1; i <= 60; i++) {
+            res.push({value: (i - 1), label: i + " сек."})
+        }
+        return res
+    }
+
+    const switchChange = (index, value) => {
+        setEnable(false)
+        stopPressed()
+        bitsInSecond[index] = bitsInSecond[index] ^ 1
+    }
+
+    const startPressed = () => {
+        if (myInterval !== null) {
+            clearInterval(myInterval)
+        }
+        let sec = 1
+        setCurrentSecond(1)
+        console.log(bitsInSecond[sec - 1])
+        bridge.send("VKWebAppFlashSetLevel", {"level": bitsInSecond[sec - 1]});
+        let timerId = setInterval(() => {
+            sec = 1 + (sec) % (bitsSize)
+            setCurrentSecond(sec)
+            console.log(bitsInSecond[sec - 1])
+            bridge.send("VKWebAppFlashSetLevel", {"level": bitsInSecond[sec - 1]});
+        }, 1000);
+        setMyInterval(timerId)
+    }
+
+    const stopPressed = () => {
+        clearInterval(myInterval)
+        setCurrentSecond(0)
+        console.log("Stop!")
+        bridge.send("VKWebAppFlashSetLevel", {"level": 0});
+    }
+
+    return <Panel id={id}>
+        <PanelHeader>Один-ноль фонарик</PanelHeader>
+        <Group>
+            <FormItem top="Количество секунд">
+                <Select
+                    placeholder="Выбрать"
+                    options={getSecondOptions()}
+                    value={bitsSize - 1}
+                    onChange={(e) => {
+                        setBitsSize(parseInt(e.target.value) + 1)
+                        setBitsInSecond(Array.from({length: parseInt(e.target.value) + 1}, (v, i) => 0))
+                        stopPressed()
+                        setEnable(false)
+                    }}
+                />
+            </FormItem>
+            <Div style={{display: 'flex'}}>
+                <Button size="l" stretched style={{marginRight: 8}} onClick={startPressed}>Начать</Button>
+                <Button size="l" stretched mode="secondary" onClick={stopPressed}>Остановить</Button>
+            </Div>
+        </Group>
+        <Group>
+            <Header mode="secondary">Управление секундами</Header>
+            {bitsInSecond.map((bool, index) => {
+                return <SimpleCell key={index}
+                   after={<Switch onChange={(e) => {switchChange(index, e.target.value); }}/>}
+                    style={{backgroundColor: index + 1 === currentSecond? "#D7FDDB": "#FFFFFF"}}
+                >
+                    {getString(index)}
+                </SimpleCell>
+            })
+            }
+        </Group>
+    </Panel>
+}
 
 export default Home;
